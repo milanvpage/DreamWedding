ecb17b769991f1bcc1ce4e0e9cdcf3341159edbc
Milan <milanvpage@gmail.com>
2021-08-03 16:57:58 -0700

    generate resources for models etc.

---
+++ app/models/wedding.rb
@@ -0,0 +1,4 @@
+class Wedding < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
---
+++ db/migrate/20210803231328_create_weddings.rb
@@ -0,0 +1,12 @@
+class CreateWeddings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :weddings do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
