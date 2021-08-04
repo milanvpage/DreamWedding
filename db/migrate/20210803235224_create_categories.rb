53aaa2eedf5e4a82a3a16f5907bef6bb627ae4e6
Milan <milanvpage@gmail.com>
2021-08-03 17:00:04 -0700

    edit category table, should'nt be a belongs_to model/table

--- db/migrate/20210803235224_create_categories.rb
+++ db/migrate/20210803235224_create_categories.rb
@@ -3,7 +3,6 @@ class CreateCategories < ActiveRecord::Migration[6.1]
     create_table :categories do |t|
       t.string :name
       t.string :season
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
