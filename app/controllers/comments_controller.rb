63a2123d946d1aa7b7767106d3cc8122a86e6a9b
Milan <milanvpage@gmail.com>
2021-08-05 15:34:09 -0700

    created controller MVC methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    def new
+    end
+    
+    def create
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def index
+    end
 end
