3aeb0d77cfa297c848d39ff484a69c17706ff15e
Milan <milanvpage@gmail.com>
2021-08-05 15:56:09 -0700

    create wedding controller methods

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,17 +1,55 @@
 class WeddingsController < ApplicationController
+
     def new
+        @wedding = Wedding.new
     end
     
     def create
-    end
+        @wedding = Wedding.new(wedding_params) 
+        if @wedding.save
+            
+            session[:wedding_id] = @wedding.id 
+            
+            redirect_to @wedding
 
-    def edit
+        else
+            render :new
+        end
     end
 
     def show
+        @wedding = Wedding.find_by_id(params[:id])
     end
 
     def index
+        @weddings = Wedding.all
     end
-    
+
+    def edit
+        @wedding = Wedding.find_by_id(params[:id])
+    end
+
+    def update
+        @wedding = Wedding.find_by_id(params[:id])
+        @wedding.update[wedding_params]
+        if @wedding.valid?
+            redirect_to wedding_path(@wedding)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @wedding = Wedding.find_by_id(params[:id])
+        @wedding.destroy
+        redirect_to weddings_path
+    end
+
+
+    private
+
+    def wedding_params
+        params.require(:wedding).permit(:content)
+    end
+
 end
