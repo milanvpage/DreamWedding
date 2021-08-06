9b0311c51fe6f1a146ec34cb79bc43cbe078a245
Milan <milanvpage@gmail.com>
2021-08-05 21:32:10 -0700

    create comment controller MVC methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,56 @@
 class CommentsController < ApplicationController
     def new
+        @comment = Comment.new
     end
     
     def create
-    end
+        @comment = Comment.new(comment_params) 
+        if @comment.save
+            
+            session[:wedding_id] = @comment.id 
+            
+            redirect_to @comment
 
-    def edit
+        else
+            render :new
+        end
     end
 
     def show
+        @comment = Comment.find_by_id(params[:id])
     end
 
     def index
+        @weddings = Comment.all
     end
+
+    def edit
+        @comment = Comment.find_by_id(params[:id])
+    end
+
+    def update
+        @comment = Comment.find_by_id(params[:id])
+        @comment.update[comment_params]
+        if @comment.valid?
+            redirect_to comment_path(@comment)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @comment = Comment.find_by_id(params[:id])
+        @comment.destroy
+        redirect_to comments_path
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+
+
 end
