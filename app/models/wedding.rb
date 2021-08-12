ad12ce733be13daa88ec1b037048331f99bb775d
Milan <milanvpage@gmail.com>
2021-08-12 11:32:43 -0700

    edit wedding CRUD action in views

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -3,4 +3,10 @@ class Wedding < ApplicationRecord
   belongs_to :venue
   has_many :comments
   has_many :users, through: :comments
+
+  def name_and_color_scheme
+    "#{self.name} - #{self.color_scheme}"
+  end
+
+  
 end
